17253828cc4855632787de3a1c57dde11adb0385
Priyansh2001here <57267750+Priyansh2001here@users.noreply.github.com>
2020-06-17 18:35:08 +0530

    Update jarvis.py
    Added feature to evaluate expression

--- jarvis.py
+++ jarvis.py
@@ -8,6 +8,17 @@ import webbrowser as wb
 import os
 import wikipedia
 import pyautogui
+import difflib
+import psutil
+
+operator = {
+    "plus": "+",
+    "minus": "-",
+    "multiply":"*",
+    "divide": "/",
+    "multiplied by": "*",
+    "divided by": "/",
+}
 
 engine=pyttsx3.init()
 def speak(audio):
@@ -124,7 +135,29 @@ def remember1():
         remember.close()
         speak("remembered sir")
     else:
-        remember1()                             
+        remember1() 
+
+def calculate():
+    speak("say an expression")
+    query=takecommand()
+
+    if "who" in query:
+        query = query.replace("who", "2")
+
+    if "divided by" in query:
+        query = query.replace("divided by", "/")
+        print(query)
+    if "x" in query:
+        query = query.replace("x","*")
+    try: 
+        x = eval(query)
+    except Exception as e:
+        speak("Err occured")
+        x = None
+    if str(x) != "None":
+        speak("your answer is "  + str(x))
+        print("your answer is "  + str(x))
+
 
 def takecommand():
     r=sr.Recognizer()
@@ -147,42 +180,23 @@ if __name__=="__main__":
     while True:
         query=takecommand().lower()
         print(query)
-        '''
-            myDict = {
-                "time":time,
-                "date":date,
-                "camera":opencam,
-                "notepad":note,
-                "shutdown":shut,
-                "restart":restart,
-                "wordpad":wordpad,
-                "screenshot":screen,
-                "logout":log,
-                "windows":start,
-                "lock":caps
-                "code":startcode,
-                "jokes":jokes,
-                "cpu":cpu,
-                "google":google,
-                "remember":remember1
-            }
-            for i in myDict.keys():
-                if i in query:
-                    myDict[i]()
-                
-        '''
-        
+        l=["time","date","camera","sleep","notepad","shutdown","restart","wordpad","wikipedia","screenshot","logout","windows","lock","thank","code","chrome","jokes","cpu","google","remember","know","calculate"]
+        query1=difflib.get_close_matches(query,l,n=1)
+        query1="".join(query1)
+        if query1 in l:
+            query=query1
+        else:
+            query=query        
+        print(query)
         if "time" in query:
             time()
         elif "date" in query:
             date()
         elif "camera" in query:
             opencam() 
-        ####
         elif "sleep" in query:
             speak("good bye sir")
             quit() 
-        ####
         elif "notepad" in query:
             note() 
         elif "shutdown" in query:
@@ -191,10 +205,8 @@ if __name__=="__main__":
             restart()
         elif "wordpad" in query:
             wordpad()
-        #####
         elif "wikipedia" in query:
             wiki(query)
-        #####
         elif "screenshot" in query:
             screen()
         elif "logout" in query:
@@ -207,8 +219,8 @@ if __name__=="__main__":
             speak("its ok sir")
         elif "code" in query:
             startcode()
-        elif "firefox" in query:
-            os.system(command="start firefox.exe") 
+        elif "chrome" in query:
+            os.system(command='C:\\"Program Files (x86)"\\Google\\Chrome\\Application\\chrome.exe') 
         elif "jokes" in query:
             jokes() 
         elif "cpu" in query:
@@ -218,10 +230,13 @@ if __name__=="__main__":
 
         elif "remember" in query:
             remember1()
-###
+
         elif "know" in query:
             remember=open('data.txt','r')
-            speak(remember.read())        
-###
+            speak(remember.read())
+
+        elif "calculate" in query:
+            calculate()             
+
             
                     
